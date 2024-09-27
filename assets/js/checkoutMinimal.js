517b63246050a49af8ac6b92fbf252d07c97bfd4
Sergio Gómez <sergioagmoreno@gmail.com>
2024-09-27 17:03:23 -0500

    Enough for the day

--- assets/js/checkoutMinimal.js
+++ assets/js/checkoutMinimal.js
@@ -4,7 +4,28 @@ jQuery(window).load( function() {
         jQuery('#summaryDetailsButton, .summaryDetails').toggleClass('open')
     })
 
-    jQuery('.addonbtn').click( function() {
+    jQuery('.addonInput').each( function() {
+        let minusButton = jQuery(this).find(".addonLess");
+        let plusButton = jQuery(this).find(".addonPlus");
+        let input = jQuery(this).find(".addonNumberInput");
+        
+        minusButton.click(function(event) {
+            event.preventDefault();
+            let inputValue = parseInt(input.val()); // Get current value each click
+
+            if (inputValue > 0) input.val(inputValue - 1);
+        });
+
+        plusButton.click(function(event) {
+            event.preventDefault();
+            let inputValue = parseInt(input.val());
+
+            input.val(inputValue + 1);
+        });
+    })
+
+    jQuery('.addonbtn').click( function(event) {
+        event.preventDefault();
         let thisText = jQuery(this).text();
 
         if (thisText === "Add") {
