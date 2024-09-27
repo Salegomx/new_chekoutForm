776e7b62fc7e111ca61d1b48a39c5a25dee801b9
Sergio Gómez <sergioagmoreno@gmail.com>
2024-09-26 23:29:45 -0500

    Will continue to work on GitHub. Enough for today

--- assets/js/checkoutMinimal.js
+++ assets/js/checkoutMinimal.js
@@ -4,4 +4,16 @@ jQuery(window).load( function() {
         jQuery('#summaryDetailsButton, .summaryDetails').toggleClass('open')
     })
 
+    jQuery('.addonbtn').click( function() {
+        let thisText = jQuery(this).text();
+
+        if (thisText === "Add") {
+            jQuery(this).text("Added!");
+        } else {
+            jQuery(this).text("Add");
+        }
+
+        jQuery(this).toggleClass('checked');
+    })
+
 })
